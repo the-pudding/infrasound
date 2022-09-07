dc39cf05a85bf78d8bfdb4732fd4958151a6f716
Jan Diehm <jadiehm@gmail.com>
2022-09-07 12:11:36 -0500

    first go at web audio api

--- src/components/demo/Demo.svelte
+++ src/components/demo/Demo.svelte
@@ -1,20 +1,75 @@
 <script>
-	import DemoLink from "$components/demo/Demo.Link.svelte";
-	import DemoImg from "$components/demo/Demo.Img.svelte";
-	import DemoButtonSet from "$components/demo/Demo.ButtonSet.svelte";
-	import DemoRange from "$components/demo/Demo.Range.svelte";
-	import DemoSelect from "$components/demo/Demo.Select.svelte";
-	import DemoToggle from "$components/demo/Demo.Toggle.svelte";
-	import DemoSortTable from "$components/demo/Demo.SortTable.svelte";
-	import DemoLayerCake from "$components/demo/Demo.LayerCake.svelte";
-	import DemoSvelteElement from "$components/demo/Demo.SvelteElement.svelte";
-	import DemoSvelteComponent from "$components/demo/Demo.SvelteComponent.svelte";
-	import DemoScrolly from "$components/demo/Demo.Scrolly.svelte";
+	// import DemoLink from "$components/demo/Demo.Link.svelte";
+	// import DemoImg from "$components/demo/Demo.Img.svelte";
+	// import DemoButtonSet from "$components/demo/Demo.ButtonSet.svelte";
+	// import DemoRange from "$components/demo/Demo.Range.svelte";
+	// import DemoSelect from "$components/demo/Demo.Select.svelte";
+	// import DemoToggle from "$components/demo/Demo.Toggle.svelte";
+	// import DemoSortTable from "$components/demo/Demo.SortTable.svelte";
+	// import DemoLayerCake from "$components/demo/Demo.LayerCake.svelte";
+	// import DemoSvelteElement from "$components/demo/Demo.SvelteElement.svelte";
+	// import DemoSvelteComponent from "$components/demo/Demo.SvelteComponent.svelte";
+	// import DemoScrolly from "$components/demo/Demo.Scrolly.svelte";
+	import { onMount } from "svelte";
+	import audioFile from "$data/horror.mp3";
+
+	let AudioContext;
+	let audioCtx;
+	let analyser;
+	let audioHTML;
+	let source;
+	let filter;
+	let isPlaying = false;
+
+	onMount(() => {
+		audioHTML = document.getElementById('audio-file');
+	});
+
+	function setupCtx() {
+		AudioContext = window.AudioContext || window.webkitAudioContext;
+		audioCtx = new AudioContext();
+	}
+
+	function setupSource() {
+		if(!source) {
+			source = audioCtx.createMediaElementSource(audioHTML);
+			analyser = audioCtx.createAnalyser();
+
+			setupFilter();
+
+			source.connect(analyser);
+			source.connect(filter)
+			filter.connect(audioCtx.destination);
+			console.log(source, analyser, audioCtx.destination);
+		}
+	}
+
+	function setupFilter() {
+		filter = audioCtx.createBiquadFilter();
+		filter.type = "bandpass"
+		filter.frequency.value = 2000;
+	}
+
+	// Create web audio api context
+	function handleClick() {
+		setupCtx();
+		setupSource();
+		if (isPlaying) {
+			audioHTML.pause();
+		} else {
+			audioHTML.play();
+		}
+		isPlaying = !isPlaying;
+	}
 </script>
 
 <div id="demo">
 	<h1>Demo</h1>
-	<DemoLink />
+	<audio id="audio-file">
+		<source src={audioFile} type="audio/mpeg">
+	</audio>
+	<button on:click="{handleClick}">Play sound</button>
+	<!-- <DemoLink />
 	<DemoImg />
 	<DemoButtonSet />
 	<DemoRange />
@@ -24,7 +79,7 @@
 	<DemoLayerCake />
 	<DemoSvelteElement />
 	<DemoSvelteComponent />
-	<DemoScrolly />
+	<DemoScrolly /> -->
 </div>
 
 <style>
